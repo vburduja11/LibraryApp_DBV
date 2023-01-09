89a4e2b5009bc16ff6e3b86f37c29f254f7f65e5
jamaldemir <jamal@cybertekschool.com>
2023-01-09 14:09:45 -0500

    login.feature and scenario outline is completed

--- src/test/java/com/cydeo/steps/LoginStepDefs.java
+++ src/test/java/com/cydeo/steps/LoginStepDefs.java
@@ -12,11 +12,14 @@ public class LoginStepDefs {
     LoginPage loginPage = new LoginPage();
     DashBoardPage dashBoardPage = new DashBoardPage();
     String actualUserName;
+    String email;
 
     @Given("the user logged in  {string} and {string}")
     public void the_user_logged_in_and(String email, String password) {
         loginPage.login(email,password);
         BrowserUtil.waitFor(2);
+        //get email assign to global
+        this.email = email;
 
     }
     @When("user gets username  from user fields")
@@ -28,7 +31,7 @@ public class LoginStepDefs {
     @Then("the username should be same with database")
     public void the_username_should_be_same_with_database() {
         //get data from database
-        String query = "select full_name from users where email = 'librarian55@library'";
+        String query = "select full_name from users where email = '"+email+"'";
 
         DB_Util.runQuery(query);
 
--- src/test/resources/features/login.feature
+++ src/test/resources/features/login.feature
@@ -1,6 +1,20 @@
 Feature: Login Functionality
-  @wip @db
+
+  @db
   Scenario: Login with valid credentials
-    Given the user logged in  "librarian55@library" and "67UQi3Ol"
+    Given the user logged in  "librarian56@library" and "pBQnq0NN"
     When user gets username  from user fields
     Then the username should be same with database
+
+  @wip @db
+  Scenario Outline: Login with valid credentials <email>
+    Given the user logged in  "<email>" and "<password>"
+    When user gets username  from user fields
+    Then the username should be same with database
+    Examples:
+      | email               | password |
+      | librarian55@library | 67UQi3Ol |
+      | librarian56@library | pBQnq0NN |
+      | student5@library    | i1oDgf2d |
+      | student6@library    | NXhpXJdC |
+
